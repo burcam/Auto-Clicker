6bae20329fac44cb43c96b6cef9f40c537c78a5b
burcam <burcam@gmail.com>
2021-10-08 03:54:31 -0400

    Upgrade project files to net6.0

--- AutoClicker/AutoClicker/AutoClicker.cs
+++ AutoClicker/AutoClicker/AutoClicker.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace AutoClicker
 {
@@ -31,10 +24,15 @@ namespace AutoClicker
 
         delegate void UpdateMouseCoordLabelsCallback(string xCoord, string yCoord);
 
+        /// <summary>
+        /// Random number generator
+        /// </summary>
+        private Random random = new Random();
+
         public AutoClicker()
-        { 
+        {
             InitializeComponent();
-            InitializeWaitTimes(minTimeDefault, maxTimeDefault, minClickBeforeMouseMoveDefault, maxClickBeforeMouseMoveDefault);
+            InitializeWaitTimes();
 
             //Mouse coordinate label thread
             Thread mouseCoordinateLabelThread = new Thread(() => ContinuallyUpdateMouseCoordLabel());
@@ -42,7 +40,7 @@ namespace AutoClicker
             mouseCoordinateLabelThread.Start();
         }
 
-        private void ContinuallyUpdateMouseCoordLabel (int precisionMilliseconds = 60)
+        private void ContinuallyUpdateMouseCoordLabel(int precisionMilliseconds = 60)
         {
             while (true)
             {
@@ -55,29 +53,25 @@ namespace AutoClicker
 
                 try
                 {
-                    this.Invoke(updateMouseCoordInvoker, new object[] { currentXCoord, currentYCoord });
+                    Invoke(updateMouseCoordInvoker, new object[] { currentXCoord, currentYCoord });
                 }
                 catch (Exception) { };
             }
         }
 
         /// <summary>
-        /// Set the min/max wait time and mouse movement clicks. 
+        /// Set the min/max wait time and mouse movement clicks to default values. 
         /// Min wait time indicates the minimum amount of time waited before the next click. 
         /// Max wait time indicates the maximum amount of time waitied before the next click.
         /// minClickBeforeMouseMove indicates the minimum number of clicks needed before mouse jumps position.
         /// maxClicksBeforeMouseMove indicates the maximum number of clicks needed before mouse jumps position.
         /// </summary>
-        /// <param name="minTime"></param>
-        /// <param name="maxTime"></param>
-        /// <param name="minClicksBeforeMouseMove"></param>
-        /// <param name="maxClicksBeforeMouseMove"></param>
-        private void InitializeWaitTimes(int minTime, int maxTime, int minClicksBeforeMouseMove, int maxClicksBeforeMouseMove)
+        private void InitializeWaitTimes()
         {
-            this.minWait.Text = minTimeDefault + "";
-            this.maxWait.Text = maxTimeDefault + "";
-            this.minClicksBetweenMovement.Text = minClickBeforeMouseMoveDefault + "";
-            this.maxClicksBetweenMovement.Text = maxClickBeforeMouseMoveDefault + "";
+            minWait.Text = minTimeDefault.ToString();
+            maxWait.Text = maxTimeDefault.ToString();
+            minClicksBetweenMovement.Text = minClickBeforeMouseMoveDefault.ToString();
+            maxClicksBetweenMovement.Text = maxClickBeforeMouseMoveDefault.ToString();
         }
 
         private void StartButton_Click(object sender, EventArgs e)
@@ -94,31 +88,28 @@ namespace AutoClicker
 
             if (ValidFieldData(out errorType))
             {
-                if (!this.run)
+                if (!run)
                     AutoClickOnNewThread();
-                this.run = true;
+                run = true;
                 DisableSettingFields();
             }
             else
             {
                 string errorMessage;
                 string caption;
-                MessageBoxButtons errorButtons = MessageBoxButtons.OK;
 
                 if (errorType == "NON-INT")
                 {
                     errorMessage = "Wait Time and Mouse Clicks need to be non-decimal numbers!";
                     caption = "Invalid input(s)";
-                    errorButtons = MessageBoxButtons.OK;
                 }
                 else
                 {
                     errorMessage = "Max clicks/Max time cannot be less than Min clicks/Min time!";
                     caption = "Max < Min Error";
-                    errorButtons = MessageBoxButtons.OK;
                 }
 
-                MessageBox.Show(errorMessage, caption, errorButtons);
+                MessageBox.Show(errorMessage, caption, MessageBoxButtons.OK);
 
             }
         }
@@ -131,19 +122,12 @@ namespace AutoClicker
         {
             typeError = "NONE";
 
-            int minClicks;
-            int maxClicks;
-            int minWait;
-            int maxWait;
-
-            try
-            {
-                minClicks = int.Parse(this.minClicksBetweenMovement.Text.ToString());
-                maxClicks = int.Parse(this.maxClicksBetweenMovement.Text.ToString());
-                minWait = int.Parse(this.minWait.Text.ToString());
-                maxWait = int.Parse(this.maxWait.Text.ToString());
-            }
-            catch (Exception)
+            if (!(
+                int.TryParse(minClicksBetweenMovement.Text, out int minClicks) &&
+                int.TryParse(maxClicksBetweenMovement.Text, out int maxClicks) &&
+                int.TryParse(this.minWait.Text, out int minWait) &&
+                int.TryParse(this.maxWait.Text, out int maxWait)
+                ))
             {
                 typeError = "NON-INT";
                 return false;
@@ -170,31 +154,29 @@ namespace AutoClicker
             t.IsBackground = true;
             t.Start();
         }
-
         private void AutoClick()
         {
 
-            int minWaitTime = int.Parse(minWait.Text.ToString());
-            int maxWaitTime = int.Parse(maxWait.Text.ToString());
+            int minWaitTime = int.Parse(minWait.Text);
+            int maxWaitTime = int.Parse(maxWait.Text);
 
-            int minClicks = int.Parse(minClicksBetweenMovement.Text.ToString());
-            int maxClicks = int.Parse(maxClicksBetweenMovement.Text.ToString());
+            int minClicks = int.Parse(minClicksBetweenMovement.Text);
+            int maxClicks = int.Parse(maxClicksBetweenMovement.Text);
 
-            while (this.run)
+            while (run)
             {
-                Random rnd = new Random();
-                int numbersToClickBeforeUpdatingPosition = rnd.Next(minClicks, maxClicks); //Number of times to click before changing mouse position
+                int numbersToClickBeforeUpdatingPosition = random.Next(minClicks, maxClicks); //Number of times to click before changing mouse position
                 int counter = 0;
 
                 //Number of clicks before randomly moving mouse position.
-                while (counter < numbersToClickBeforeUpdatingPosition && this.run)
+                while (counter < numbersToClickBeforeUpdatingPosition && run)
                 {
-                    int timeBetweenClicks = rnd.Next(minWaitTime, maxWaitTime);
+                    int timeBetweenClicks = random.Next(minWaitTime, maxWaitTime);
                     DoMouseClick();
-                    this.mouseClicksThisRun++; //Keep track of mouse clicks since user started clicker. 
+                    mouseClicksThisRun++; //Keep track of mouse clicks since user started clicker. 
 
-                    MethodInvoker numClicksLabelUpdater = new MethodInvoker(() => SetMouseClickLabel(this.mouseClicksThisRun));
-                    this.Invoke(numClicksLabelUpdater);
+                    MethodInvoker numClicksLabelUpdater = new MethodInvoker(() => SetMouseClickLabel(mouseClicksThisRun));
+                    Invoke(numClicksLabelUpdater);
 
                     Thread.Sleep(timeBetweenClicks);
                     counter++;
@@ -202,7 +184,7 @@ namespace AutoClicker
 
                 //Also need an auto clicking thing.
                 MethodInvoker mouseCoordUpdateInvoker = new MethodInvoker(RandomlyUpdateMouseCoordinates);
-                this.Invoke(mouseCoordUpdateInvoker);
+                Invoke(mouseCoordUpdateInvoker);
 
                 Thread.Sleep(1000);
             }
@@ -216,19 +198,18 @@ namespace AutoClicker
         private void RandomlyUpdateMouseCoordinates()
         {
 
-            if (this.run)
+            if (run)
             {
                 UpdateMouseCenterIfUserMovedMouse();
-             
-                Random rnd = new Random();
-                int randX = this.centerMouseX + rnd.Next(1, mousePixelsToMoveFromCenter);
-                int randY = this.centerMouseY + rnd.Next(1, mousePixelsToMoveFromCenter);
+
+                int randX = centerMouseX + random.Next(1, mousePixelsToMoveFromCenter);
+                int randY = centerMouseY + random.Next(1, mousePixelsToMoveFromCenter);
 
                 Win32.POINT p = new Win32.POINT();
 
-                int sleepTime = rnd.Next(1000, 4500); //Sleep between 1 and 4.5 seconds..?
+                int sleepTime = random.Next(1000, 4500); //Sleep between 1 and 4.5 seconds..?
 
-                Win32.ClientToScreen(this.Handle, ref p);
+                Win32.ClientToScreen(Handle, ref p);
                 Win32.SetCursorPos(randX, randY);
                 System.Threading.Thread.Sleep(sleepTime);
 
@@ -245,8 +226,8 @@ namespace AutoClicker
         /// <param name="yPos"></param>
         private void SetMouseCoordLabels(string xPos, string yPos)
         {
-            this.MouseXLabel.Text = "Mouse X Position: " + xPos;
-            this.MouseYLabel.Text = "Mouse Y position: " + yPos;
+            //this.MouseXLabel.Text = "Mouse X Position: " + xPos;
+            //this.MouseYLabel.Text = "Mouse Y position: " + yPos;
         }
 
         /// <summary>
@@ -254,7 +235,7 @@ namespace AutoClicker
         /// </summary>
         private void DoMouseClick()
         {
-            if (this.run)
+            if (run)
             {
                 //Call the imported function with the cursor's current position
                 uint X = (uint)Cursor.Position.X;
@@ -269,7 +250,7 @@ namespace AutoClicker
         /// </summary>
         private void UpdateMouseCenterIfUserMovedMouse()
         {
-            if (Math.Abs(Cursor.Position.X - centerMouseX) > mousePixelsToMoveFromCenter 
+            if (Math.Abs(Cursor.Position.X - centerMouseX) > mousePixelsToMoveFromCenter
                 || Math.Abs(Cursor.Position.Y - centerMouseY) > mousePixelsToMoveFromCenter)
             {
                 centerMouseX = Cursor.Position.X;
@@ -307,13 +288,13 @@ namespace AutoClicker
         /// <param name="numClicks"></param>
         private void SetMouseClickLabel(int numClicks)
         {
-            this.numMouseClicks.Text = numClicks + " Total Mouse Clicks";
+            this.numMouseClicks.Text = $"{numClicks} Total Mouse Clicks";
         }
 
         private void StopButton_Click(object sender, EventArgs e)
         {
-            this.run = false;
-            this.mouseClicksThisRun = 0;
+            run = false;
+            mouseClicksThisRun = 0;
             SetMouseClickLabel(mouseClicksThisRun);
             EnableSettingFields();
         }
@@ -329,11 +310,11 @@ namespace AutoClicker
         /// </summary>
         private void EnableSettingFields()
         {
-            this.startButton.Enabled = true;
-            this.maxClicksBetweenMovement.Enabled = true;
-            this.minClicksBetweenMovement.Enabled = true;
-            this.minWait.Enabled = true;
-            this.maxWait.Enabled = true;
+            startButton.Enabled = true;
+            maxClicksBetweenMovement.Enabled = true;
+            minClicksBetweenMovement.Enabled = true;
+            minWait.Enabled = true;
+            maxWait.Enabled = true;
         }
 
         /// <summary>
@@ -345,13 +326,17 @@ namespace AutoClicker
         /// </summary>
         private void DisableSettingFields()
         {
-            this.startButton.Enabled = false;
-            this.maxClicksBetweenMovement.Enabled = false;
-            this.minClicksBetweenMovement.Enabled = false;
-            this.minWait.Enabled = false;
-            this.maxWait.Enabled = false;
+            startButton.Enabled = false;
+            maxClicksBetweenMovement.Enabled = false;
+            minClicksBetweenMovement.Enabled = false;
+            minWait.Enabled = false;
+            maxWait.Enabled = false;
         }
         #endregion
 
+        private void AutoClicker_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- AutoClicker/AutoClicker/Program.cs
+++ AutoClicker/AutoClicker/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,16 +9,15 @@ namespace AutoClicker
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            AutoClicker autoclicker = new AutoClicker();
-            autoclicker.Text = "Auto Clicker";
-            Application.Run(autoclicker);
+            Application.Run(new AutoClicker());
         }
     }
 }
